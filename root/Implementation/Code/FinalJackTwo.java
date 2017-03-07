c1f27479435517dac91b78cb66484c535b15ebb9
Jamie Slome <jamieslome@Jamies-MacBook-Air.local>
2017-03-07 12:59:16 +0000

    Pushing updated code

--- root/Implementation/Code/FinalJackTwo.java
+++ root/Implementation/Code/FinalJackTwo.java
@@ -1,45 +1,113 @@
 package leJOSEV3;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.utility.Delay;
+import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.Color;
 import lejos.robotics.SampleProvider;
 
 public class FinalJackTwo {
     
-    static EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.A);
-    static EV3LargeRegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.D);
-    static EV3LargeRegulatedMotor[] toSynchronise = new EV3LargeRegulatedMotor[]{motorRight};
-    static double wheelCircumference=2*2.75*Math.PI;
-    static EV3GyroSensor gyroSensor = new EV3GyroSensor(SensorPort.S1);
-    static SampleProvider angleProvider = gyroSensor.getAngleMode();
+    static EV3LargeRegulatedMotor motorLeft;
+    static EV3LargeRegulatedMotor motorRight;
+    static EV3LargeRegulatedMotor[] toSynchronise;
+    static double wheelCircumference;
+    static EV3GyroSensor gyroSensor;
+    static SampleProvider angleProvider;
     static double currentAngle;
     static double errorAngle;
-    static double errorLastAngle = 0;
-    static double kP = 50;
-    static double kD = 50;
+    static double errorLastAngle;
+    static double kP;
+    static double kD;
     static double derivativeAngle;
     static double correctionAngle;
-    static double RIGHT_ANGLE = 90.0;
-    static float[] angleSamples = new float[1];
+    static double RIGHT_ANGLE;
+    static float[] angleSamples;
+    
+    static boolean[] lineValues;
+    static boolean notLocalised;
+    static SampleProvider colourProvider;
+    static EV3ColorSensor colorSensor;
+    static boolean lightValue;
+    static float[] lightSamples;
+    static HashMap<Integer, Double> lineProbabilities;
+    static double normValue;
+    static double totalProb;
+    
+    //Test probabilities...
+    static double sensorWorks;
+    static double sensorNot;
+    static double motorCorrect;
+    static double motorWrong;
+    
+    static EV3TouchSensor impactSensor;
+    static SampleProvider impactProvider;
+    static float[] impactSamples;
+    static float impactValue;
+    
+    static EV3UltrasonicSensor soundSensor;
+    static SampleProvider soundProvider;
+    static float[] soundSamples;
+    static float soundValue;
+    
+    static int stateTracker;
     
     public static void initialiser() {
         motorLeft = new EV3LargeRegulatedMotor(MotorPort.A);
         motorRight = new EV3LargeRegulatedMotor(MotorPort.D);
         toSynchronise = new EV3LargeRegulatedMotor[]{motorRight};
         
-        gyroSensor = new EV3GyroSensor(SensorPort.S3);
+        gyroSensor = new EV3GyroSensor(SensorPort.S1);
         wheelCircumference = 2*2.75*Math.PI;
         angleProvider = gyroSensor.getAngleMode();
         errorLastAngle = 0;
-        kP = 50;
-        kD = 50;
-        RIGHT_ANGLE = 90.0;
+        kP = 0.1;
+        kD = 0.1;
+        //RIGHT_ANGLE = 90.0;
         angleSamples = new float[1];
+        lightSamples = new float[1];
+        impactSamples = new float[1];
+        
+        motorLeft.setSpeed(90);
+        motorRight.setSpeed(90);
+        motorLeft.setAcceleration(2000);
+        motorRight.setAcceleration(2000);
+        
+        //True is BLUE - False is WHITE
+        lineValues = new boolean[]{true, false, true, false, false, true, true, false, true, true, false, false, true, true, true, false, true, true, true, false, false, true, true, true, false, false, false, true, true, true, true, false, false, false};;
+        colorSensor = new EV3ColorSensor(SensorPort.S4);
+        colourProvider = colorSensor.getRedMode();
+        notLocalised = true;
+        lineProbabilities = new HashMap<Integer, Double>();
+        for(int i =0; i < 34; i++) {
+            lineProbabilities.put(i, (double) 1/34);
+        }
+        normValue = 1;
+        sensorWorks = 0.98;
+        sensorNot = 0.02;
+        
+        impactSensor = new EV3TouchSensor(SensorPort.S3);
+        impactProvider = impactSensor.getTouchMode();
+        
+        soundSensor = new EV3UltrasonicSensor(SensorPort.S2);
+        soundProvider = soundSensor.getDistanceMode();
+        
+        stateTracker = -1;
+        stateTracker++;
+        
     }
     
     public static void moveForward(double distance){
@@ -70,25 +138,57 @@ public class FinalJackTwo {
         motorRight.waitComplete();
     }
     
-    public static void rotateLeft() {
+    public static void rotateLeft(double targetAngle) {
         gyroSensor.reset();
         while(true){
+            
+            
             currentAngle = readAngle(angleSamples);
-            errorAngle = RIGHT_ANGLE - currentAngle;
+            System.out.println(currentAngle);
+            errorAngle = targetAngle - currentAngle;
             derivativeAngle = errorAngle + errorLastAngle;
             correctionAngle = kP*errorAngle + kD*derivativeAngle;
             
             motorLeft.synchronizeWith(new EV3LargeRegulatedMotor[]{motorRight});
             motorLeft.startSynchronization();
             
-            motorLeft.setSpeed((int) correctionAngle * -1);
+            motorLeft.setSpeed((int) correctionAngle);
             motorRight.setSpeed((int) correctionAngle);
-            
-            motorLeft.forward();
-            motorRight.forward();
+            if(currentAngle > targetAngle) {
+                motorLeft.backward();
+                motorRight.forward();
+            } else {
+                motorLeft.forward();
+                motorRight.backward();
+            }
             
             motorLeft.endSynchronization();
             errorLastAngle = errorAngle;
+            
+        }
+    }
+    
+    public static void rotateRight(double targetAngle) {
+        gyroSensor.reset();
+        while(true){
+            currentAngle = readAngle(angleSamples);
+            System.out.println(currentAngle);
+            errorAngle = targetAngle - currentAngle;
+            derivativeAngle = errorAngle + errorLastAngle;
+            correctionAngle = kP*errorAngle + kD*derivativeAngle;
+            
+            motorLeft.synchronizeWith(new EV3LargeRegulatedMotor[]{motorRight});
+            motorLeft.startSynchronization();
+            
+            motorLeft.setSpeed((int) correctionAngle);
+            motorRight.setSpeed((int) correctionAngle);
+            
+            motorLeft.forward();
+            motorRight.backward();
+            
+            motorLeft.endSynchronization();
+            errorLastAngle = errorAngle;
+            
         }
     }
     
@@ -96,16 +196,136 @@ public class FinalJackTwo {
         angleProvider.fetchSample(sampleArray, 0);
         return sampleArray[0];
     }
-
+    
+    public static float readLight(float[] sampleArray) {
+        colourProvider.fetchSample(sampleArray, 0);
+        return sampleArray[0];
+        
+    }
+    
+    public static float readImpact(float[] sampleArray) {
+        impactProvider.fetchSample(sampleArray, 0);
+        return sampleArray[0];
+    }
+    
+    public static float readSound(float[] sampleArray) {
+        soundProvider.fetchSample(sampleArray, 0);
+        return sampleArray[0];
+    }
+    
+    //Increase precision...
+    public static boolean processLight(double raw){
+        if(raw < 0.78){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    //Task One...
+    public static void localiseLine(){
+        
+        while(notLocalised) {
+            //Take a light reading...
+            lightValue = processLight(readLight(lightSamples));
+            totalProb = 0;
+            for(double entry : lineProbabilities.values()) {
+                totalProb += entry;
+            }
+            normValue = 1/totalProb;
+            
+            for(int i = 0; i < lineValues.length; i++) {
+                if(lightValue == lineValues[i]){
+                    lineProbabilities.put(i,normValue*sensorWorks*lineProbabilities.get(i));
+                }
+                else{
+                    lineProbabilities.put(i,normValue*sensorNot*lineProbabilities.get(i));
+                }
+                
+            }
+            moveForward(2);
+            for(int i = 0; i < lineValues.length; i++) {
+                double newValue;
+                if(i == 0){
+                    newValue = normValue*((lineProbabilities.get(i+1))*motorWrong + (lineProbabilities.get(i))*motorWrong);
+                } else {
+                    newValue = normValue*(lineProbabilities.get(i-1))*motorCorrect + (lineProbabilities.get(i/*+1*/))*motorWrong + (lineProbabilities.get(i))*motorWrong; 
+                }
+                lineProbabilities.put(i, newValue);
+            }
+            for(double i:lineProbabilities.keySet()){
+                System.out.print(""+i+" ");
+            }
+            
+            //for(int i =0; i < lineValues.length; i++) {
+            //if(lineProbabilities.get(i) > 0.9) {
+            //break;
+            //}
+            //}
+            
+        }
+        stateTracker++;
+    }
+    
+    //Task Two...
+    public static void potentialPlan() {
+        
+    }
+    
+    public static void impactStop() {
+        
+        impactValue = (int)(readImpact(impactSamples));
+        
+        if(impactValue == 1) {
+            stopMotors();
+            playBeep();
+        }
+    }
+    
+    public static void stopMotors() {
+        motorLeft.stop();
+        motorRight.stop();
+    }
+    
+    public static void playBeep() {
+        Sound.beep();
+    }
+    
     public static void main(String args[]) {
         initialiser();
-        motorLeft.setSpeed(90);
-        motorRight.setSpeed(90);
-        motorLeft.setAcceleration(2000);
-        motorRight.setAcceleration(2000);
+        //		System.out.println("test");
+        //	    Button.waitForAnyPress();
+        //		for (Map.Entry<Integer, Double> entry : lineProbabilities.entrySet()) {
+        //		    System.out.print(" "+entry.getValue() +" ");
+        //		}
+        //	    Button.waitForAnyPress();
+        //System.out.println(lineProbabilities.values());
+        //Button.waitForAnyPress();
         
-        moveForward(5);
-        Delay.msDelay(1000);
-        moveBackward(5);
-    }   
+        localiseLine();
+        
+        
+        /*initialiser();
+         while(true){
+         
+         if(stateTracker == 0){
+         
+         localiseLine();
+         
+         }
+         
+         else if(stateTracker == 1){
+         
+         potentialPlan();
+         
+         }
+         
+         else if(stateTracker == 2){
+         
+         }
+         }*/
+    }
+    
 }
+
